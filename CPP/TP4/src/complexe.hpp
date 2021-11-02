86155b577d588916467cca5e4269bf9d05432b2d
Roxane <roxane.deflandre@etu.uca.fr>
2021-11-02 09:04:46 +0100

    Test n°2

--- CPP/TP4/src/complexe.hpp
+++ CPP/TP4/src/complexe.hpp
@@ -8,40 +8,40 @@
 
 // Classe  C o m p l e x e //-----------------------------------------------------------------------
 struct Complexe {
- double reel;
- double imaginaire;
+    double reel;
+    double imaginaire;
 
- Complexe(double r = 0.0,double i = 0.0) : reel(r),imaginaire(i) {}
+    Complexe(double r = 0.0, double i = 0.0) : reel(r), imaginaire(i) {}
 };
 
 // Mouchard ? //------------------------------------------------------------------------------------
 using complexe_t = Mouchard<Complexe>;
 //using complexe_t = Complexe;
 
-// Surcharge opérateurs //--------------------------------------------------------------------------
+// Surcharge opï¿½rateurs //--------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------------------operator+
-inline complexe_t operator+(const complexe_t & a,const complexe_t & b) {
- return complexe_t(a.reel+b.reel,a.imaginaire+b.imaginaire);
+inline complexe_t operator+(const complexe_t &a, const complexe_t &b) {
+    return complexe_t(a.reel + b.reel, a.imaginaire + b.imaginaire);
 }
 
 //-----------------------------------------------------------------------------------------operator*
-inline complexe_t operator*(const complexe_t & a,const complexe_t & b) {
- return complexe_t(a.reel*b.reel-a.imaginaire*b.imaginaire,
-                   a.reel*b.imaginaire+a.imaginaire*b.reel);
+inline complexe_t operator*(const complexe_t &a, const complexe_t &b) {
+    return complexe_t(a.reel * b.reel - a.imaginaire * b.imaginaire,
+                      a.reel * b.imaginaire + a.imaginaire * b.reel);
 }
 
 //----------------------------------------------------------------------------------------operator<<
-inline std::ostream & operator<<(std::ostream & flux,const complexe_t & c) {
- flux << "(" << c.reel << ";" << c.imaginaire << ")";
- return flux;
+inline std::ostream &operator<<(std::ostream &flux, const complexe_t &c) {
+    flux << "(" << c.reel << ";" << c.imaginaire << ")";
+    return flux;
 }
 
 // Classe  C o m p a r a t e u r C o m p l e x e //-------------------------------------------------
 struct ComparateurComplexe {
- bool operator() (const complexe_t & a,const complexe_t & b) const {
-  return (a.reel<b.reel || (a.reel==b.reel && a.imaginaire<b.imaginaire));
- }
+    bool operator()(const complexe_t &a, const complexe_t &b) const {
+        return (a.reel < b.reel || (a.reel == b.reel && a.imaginaire < b.imaginaire));
+    }
 };
 
 // Fin //-------------------------------------------------------------------------------------------
--- CPP/TP4/test/test_mouchard.cpp
+++ CPP/TP4/test/test_mouchard.cpp
@@ -4,33 +4,37 @@
 
 // Fonction principale //---------------------------------------------------------------------------
 int main(void) {
- complexe_t::stats();
+    complexe_t::stats();
 
- std::cout << std::endl << "complexe_t c1(1,2);" << std::endl;
- complexe_t c1(1,2);
- complexe_t::stats();
+    std::cout << std::endl << "complexe_t c1(1,2);" << std::endl;
+    complexe_t c1(1, 2);            // Constructeur ad hoc
+    complexe_t::stats();
 
- std::cout << std::endl << "complexe_t c2(3,4);" << std::endl;
- complexe_t c2(3,4);
- complexe_t::stats();
+    std::cout << std::endl << "complexe_t c2(3,4);" << std::endl;
+    complexe_t c2(3, 4);            // Constructeur ad hoc
+    complexe_t::stats();
 
- std::cout << std::endl << "complexe_t c3(c1);" << std::endl;
- complexe_t c3(c1);
- complexe_t::stats();
+    std::cout << std::endl << "complexe_t c3(c1);" << std::endl;
+    complexe_t c3(c1);              // Constructeur par copie
+    complexe_t::stats();
 
- std::cout << std::endl << "complexe_t c4(std::move(c2));" << std::endl;
- complexe_t c4(std::move(c2));
- complexe_t::stats();
+    std::cout << std::endl << "complexe_t c4(std::move(c2));" << std::endl;
+    complexe_t c4(std::move(c2));   // Constructeur de mouvement
+    complexe_t::stats();
 
- std::cout << std::endl << "c3=c1;" << std::endl;
- c3=c1;
- complexe_t::stats();
+    std::cout << std::endl << "c3=c1;" << std::endl;
+    c3 = c1;                        // Affectation par copie
+    complexe_t::stats();
 
- std::cout << std::endl << "c4=complexe_t(5,6);" << std::endl;
- c4=complexe_t(5,6);
- complexe_t::stats();
+    std::cout << std::endl << "c4=complexe_t(5,6);" << std::endl;
+    c4 = complexe_t(5, 6);          // Construction ad hoc + Affectation par mouvement
+    complexe_t::stats();
 
- return 0;
+    // Affectation par mouvement : apres l'affectation, ce qui a servit pour l'affectation est soit vide
+    // soit n'existe plus du tout !
+    // Affectation par copie : apres l'affectation, ce qui a servit pour l'affectation contient existe tjrs
+
+    return 0;
 }
 
 // Fin //-------------------------------------------------------------------------------------------
